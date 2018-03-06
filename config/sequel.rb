8efdb425eed725a361475d65693db3efcaa38559
Nick A <nick@nicksit.com>
2018-03-06 09:46:53 -0800

    set up .env in .gitignore

--- config/sequel.rb
+++ config/sequel.rb
@@ -1,5 +1,5 @@
 require 'sequel'
-#DB = Sequel.sqlite("./db/#{ENV.fetch('RACK_ENV', 'development')}.db")
-DB = Sequel.connect(ENV['DATABASE_URL'])
+#for remote use Heroku
+#DB = Sequel.connect(ENV['DATABASE_URL'])
 #for local use
-#DB = Sequel.connect('postgres://user:password@localhost:5432/database')
+DB = Sequel.connect('postgres://dev-mbr:4hamD3v@localhost:5432/mbr-devdb')
