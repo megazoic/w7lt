e1f4da91cf9a3ee1df91916279fb0289c7e3c234
Nick A <nick@nicksit.com>
2018-03-14 10:14:53 -0700

    get set to push up to heroku

--- config/sequel.rb
+++ config/sequel.rb
@@ -1,5 +1,5 @@
 require 'sequel'
 #for remote use Heroku
-#DB = Sequel.connect(ENV['DATABASE_URL'])
+DB = Sequel.connect(ENV['DATABASE_URL'])
 #for local use
-DB = Sequel.connect('postgres://dev-mbr:4hamD3v@localhost:5432/mbr-devdb')
+#DB = Sequel.connect('postgres://dev-mbr:4hamD3v@localhost:5432/mbr-devdb')
