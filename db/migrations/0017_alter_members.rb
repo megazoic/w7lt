a326978f2a62182c837913ce2b46e0cf8edee560
Nick <nick@nicksit.com>
2019-02-21 21:14:55 -0800

    screwed up the migration last time

--- db/migrations/0017_alter_members.rb
+++ db/migrations/0017_alter_members.rb
@@ -2,6 +2,8 @@ Sequel.migration do
   change do
     alter_table(:members) do
       drop_column :mbr_type
+    end
+    alter_table(:auth_users) do
       drop_column :desc
       add_column :description, String
     end
