e1cd9f4c403eb90cff9436844bb8082bef1a099b
Nick <nick@nicksit.com>
2019-02-21 19:24:28 -0800

    changed connection parameter so that it is same as on Heroku

--- config/sequel.rb
+++ config/sequel.rb
@@ -1,5 +1,3 @@
 require 'sequel'
 #for remote use Heroku
-#DB = Sequel.connect(ENV['DATABASE_URL'])
-#for local use
-DB = Sequel.connect("#{ENV['PARCDBCONN']}")
+DB = Sequel.connect(ENV['DATABASE_URL'])
